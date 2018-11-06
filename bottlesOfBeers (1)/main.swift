f925a71d10f9e57e332913bcd63a4deb77ba38c7
Anne-E <aepinning@hotmail.com>
2018-11-06 12:32:26 +0100

    1st version of code done

--- bottlesOfBeers (1)/main.swift
+++ bottlesOfBeers (1)/main.swift
@@ -1,12 +1,18 @@
-//
-//  main.swift
-//  bottlesOfBeers (1)
-//
-//  Created by Anne-Elisabeth on 06/11/2018.
-//  Copyright © 2018 Anne-Elisabeth. All rights reserved.
-//
+var numberOfBottles: Int = 99
 
-import Foundation
+while (numberOfBottles != 0) {
+    if (numberOfBottles == 1) {
+        print("1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\n")
+    } else {
+        print("\(numberOfBottles) bottles of beer on the wall, \(numberOfBottles) bottles of beer.")
+        if(numberOfBottles > 2) {
+            print("Take one down and pass it around, \(numberOfBottles - 1) bottles of beer on the wall.\n")
+        } else {
+            print("Take one down and pass it around, 1 bottle of beer on the wall.\n")
+        }
+    }
+    numberOfBottles = numberOfBottles - 1
+}
 
-print("Hello, World!")
+print("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.")
 
