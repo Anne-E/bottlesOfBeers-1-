344965d532250b1a766f6ce78f808ab6cb5208e8
Anne-E <aepinning@hotmail.com>
2018-11-06 12:29:52 +0100

    Initial Commit

---
+++ bottlesOfBeers (1)/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  bottlesOfBeers (1)
+//
+//  Created by Anne-Elisabeth on 06/11/2018.
+//  Copyright © 2018 Anne-Elisabeth. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
